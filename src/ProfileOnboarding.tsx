d322d4b913a0b3ebd7b21f7b8bb79a2170aef688
Jack Fan <jack.fan.dev@gmail.com>
2025-05-29 02:07:17 -0400

    yes

--- src/ProfileOnboarding.tsx
+++ src/ProfileOnboarding.tsx
@@ -386,7 +386,7 @@ const ProfileOnboarding = ({
         };
         
         // Make API call to create user
-        const response = await fetch('http://localhost:8000/api/users', {
+        const response = await fetch('https://series-api-202642739529.us-central1.run.app/api/users', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify(userDataWithProfilePic)
